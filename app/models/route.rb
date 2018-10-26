57b643bd91f5b34f31df0d95aa70211b5fdddfe4
Mateo Avantaggiato <mateqac@Mateos-MBP.home>
2018-10-26 15:18:58 +0200

    .

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,6 +5,6 @@ class Route < ApplicationRecord
   has_one :init_location, :class_name => 'Location', :foreign_key => 'init_location_id'
   has_one :end_location, :class_name => 'Location', :foreign_key => 'end_location_id'
 
-  accepts_nested_attributes_for :init_location, reject_if: :all_blank, allow_destroy: true  
-  accepts_nested_attributes_for :end_location, reject_if: :all_blank, allow_destroy: true  
+  accepts_nested_attributes_for :init_location, :address
+  accepts_nested_attributes_for :end_location, :address
 end
