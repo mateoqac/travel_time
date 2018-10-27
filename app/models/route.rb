70d2bc0a56a0f85489f0cb188dd616210ab06c87
Mateo Avantaggiato <mateqac@Mateos-MBP.home>
2018-10-28 01:54:22 +0200

    errors

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,9 +1,9 @@
 class Route < ApplicationRecord
   belongs_to :journey
 
-  belongs_to :init_location, :class_name => 'Location'
-  belongs_to :end_location, :class_name => 'Location'
+  belongs_to :init_location, :class_name => 'Location', optional: true
+  belongs_to :end_location, :class_name => 'Location', optional: true
 
-  accepts_nested_attributes_for :init_location, reject_if: :all_blank, allow_destroy: true  
-  accepts_nested_attributes_for :end_location, reject_if: :all_blank, allow_destroy: true  
+  accepts_nested_attributes_for :init_location, allow_destroy: true, reject_if:  ->(attrs) { attrs['address'].blank?}
+  accepts_nested_attributes_for :end_location, allow_destroy: true, reject_if:  ->(attrs) { attrs['address'].blank?}  
 end
